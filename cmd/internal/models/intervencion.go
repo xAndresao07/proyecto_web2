7bbac89f682b5f641cf76b74c0392d14decad1be
Mario Soriano <mariosoriano940@gmail.com>
2026-05-25 20:57:59 -0500

    feat: crear struct del modelo Intervencion con tags JSON

---
+++ cmd/internal/models/intervencion.go
@@ -0,0 +1,11 @@
+package models
+
+// Intervencion representa la cita técnica entre un estudiante y un compañero que le brindará ayuda.
+type Intervencion struct {
+	ID             string `json:"id"`
+	SolicitanteID  string `json:"solicitante_id"`
+	TecnicoID      string `json:"tecnico_id"`
+	Estado         string `json:"estado"`
+	HoraAcordada   string `json:"hora_acordada"`
+	PuntoEncuentro string `json:"punto_encuentro"`
+}
