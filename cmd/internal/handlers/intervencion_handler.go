12df081cc491996cd12b61234926f3da8ad9c61a
Mario Soriano <mariosoriano940@gmail.com>
2026-05-25 21:31:43 -0500

    feat: agregar handlers para crear (POST) intervenciones

--- cmd/internal/handlers/intervencion_handler.go
+++ cmd/internal/handlers/intervencion_handler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"proyecto/cmd/internal/models"
 	"proyecto/cmd/internal/storage"
+	"strconv"
 )
 
 // Listamos todas las intervenciones
@@ -15,4 +17,31 @@ func GetAllIntervenciones(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(storage.Intervenciones)
+}
+
+// Creamos una intervención nueva
+
+func CreateIntervencion(w http.ResponseWriter, r *http.Request) {
+	var nueva models.Intervencion
+	if err := json.NewDecoder(r.Body).Decode(&nueva); err != nil {
+		http.Error(w, "Error decodificando la solicitud: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if nueva.SolicitanteID == "" || nueva.TecnicoID == "" || nueva.HoraAcordada == "" || nueva.PuntoEncuentro == "" {
+		http.Error(w, "SolicitanteID, TecnicoID, HoraAcordada y PuntoEncuentro son obligatorios", http.StatusBadRequest) // error 400 Bad Request
+	}
+
+	// Forzamos el estado inicial, por si el cliente intenta mandar otro estado
+	nueva.Estado = "pendiente"
+
+	storage.Mu.Lock()
+	nueva.ID = strconv.Itoa(storage.SiguienteID) // Asignamos un ID único
+	storage.SiguienteID++
+	storage.Intervenciones = append(storage.Intervenciones, nueva)
+	storage.Mu.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201 Created
+	json.NewEncoder(w).Encode(nueva)
 }
