8ae47c10c56ba0df78fb816af5f074db4f66493a
Mario Soriano <mariosoriano940@gmail.com>
2026-05-25 21:05:19 -0500

    feat: inicializar almacenamiento en memoria con slice y mutex

---
+++ cmd/internal/storage/memory.go
@@ -0,0 +1,17 @@
+package storage
+
+import (
+	"proyecto/cmd/internal/models"
+	"sync"
+)
+
+var (
+	// Mu nos ayuda a evitar errores si dos peticiones intentan guardar al mismo tiempo
+	Mu sync.Mutex
+
+	// Intervenciones es nuestro "slice" (lista) que simula la tabla de la base de datos
+	Intervenciones []models.Intervencion
+
+	// SiguienteID es un contador para asignar un ID único a cada nueva intervención
+	SiguienteID = 1
+)
