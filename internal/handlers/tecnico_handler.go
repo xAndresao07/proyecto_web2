1c429c8b8784ca97b55a0fca8a25462da4e86dbf
Anthol34 <josue10anthony1@gmail.com>
2026-06-09 01:03:19 -0500

    feat: implementa logica principal para crear un tecnico (POST)

--- internal/handlers/tecnico_handler.go
+++ internal/handlers/tecnico_handler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"proyecto/internal/models"
 	"proyecto/internal/storage"
+	"strconv"
 )
 
 func GetAllTecnicos(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +15,22 @@ func GetAllTecnicos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(storage.Tecnicos)
+}
+
+func CreateTecnico(w http.ResponseWriter, r *http.Request) {
+	var nuevo models.Tecnico
+	if err := json.NewDecoder(r.Body).Decode(&nuevo); err != nil {
+		http.Error(w, "Error decodificando: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	storage.Mu.Lock()
+	nuevo.ID = strconv.Itoa(storage.SiguienteID)
+	storage.SiguienteID++
+	storage.Tecnicos = append(storage.Tecnicos, nuevo)
+	storage.Mu.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(nuevo)
 }
