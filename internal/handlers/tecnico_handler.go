99a77ccd31e31d650b7a47ca858969aa28b6a071
Anthol34 <josue10anthony1@gmail.com>
2026-06-09 01:19:41 -0500

    feat: implementa handler UpdateTecnico para actualizacion parcial (PUT)

--- internal/handlers/tecnico_handler.go
+++ internal/handlers/tecnico_handler.go
@@ -59,3 +59,43 @@ func GetTecnicoPorID(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Error(w, "Técnico no encontrado", http.StatusNotFound)
 }
+
+func UpdateTecnico(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+
+	var datosActualizados models.Tecnico
+	if err := json.NewDecoder(r.Body).Decode(&datosActualizados); err != nil {
+		http.Error(w, "Error decodificando", http.StatusBadRequest)
+		return
+	}
+
+	storage.Mu.Lock()
+	defer storage.Mu.Unlock()
+
+	for i, tecnico := range storage.Tecnicos {
+		if tecnico.ID == idParam {
+			datosActualizados.ID = idParam
+
+			// Actualización parcial
+			if datosActualizados.Nombre == "" {
+				datosActualizados.Nombre = tecnico.Nombre
+			}
+			if len(datosActualizados.Habilidades) == 0 {
+				datosActualizados.Habilidades = tecnico.Habilidades
+			}
+			if datosActualizados.HorarioLibre == "" {
+				datosActualizados.HorarioLibre = tecnico.HorarioLibre
+			}
+			if datosActualizados.Reputacion == 0 {
+				datosActualizados.Reputacion = tecnico.Reputacion
+			}
+
+			storage.Tecnicos[i] = datosActualizados
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(datosActualizados)
+			return
+		}
+	}
+	http.Error(w, "Técnico no encontrado", http.StatusNotFound)
+}
