9632a793bb18afb6807c2f55db3daebef520e409
Mario Soriano <mariosoriano940@gmail.com>
2026-06-29 23:42:56 -0500

    feat(models): definir entidades de negocio y sincronizar nombres de tablas para gorm

--- internal/models/cita.go
+++ internal/models/cita.go
@@ -1,8 +1,9 @@
 package models
 
 // Cita representa la cita técnica entre un estudiante y un compañero que le brindará ayuda.
+
 type Cita struct {
-	ID             int    `json:"id"`
+	ID             int    `json:"id" gorm:"primaryKey"`
 	SolicitanteID  string `json:"solicitante_id"`
 	TecnicoID      string `json:"tecnico_id"`
 	Estado         string `json:"estado"`
--- internal/models/soporte.go
+++ internal/models/soporte.go
@@ -1,10 +1,11 @@
 package models
 
 // Soporte representa la auditoría y cierre del ciclo una vez terminada la cita.
+
 type Soporte struct {
-	ID              int    `json:"id"`
-	CitaID          int    `json:"cita_id"`          // Conecta con la Cita finalizada
-	DispositivoID   int    `json:"dispositivo_id"`   // Conecta con el dispositivo del módulo de Jandry
-	Solucion        string `json:"solucion"`         // Detalle técnico de lo que se reparó
-	PiezasCambiadas string `json:"piezas_cambiadas"` // Detalle de las piezas que se cambiaron, si hubo
+	ID              int    `json:"id" gorm:"primaryKey"`
+	CitaID          int    `json:"cita_id"`
+	DispositivoID   int    `json:"dispositivo_id"`
+	Solucion        string `json:"solucion"`
+	PiezasCambiadas string `json:"piezas_cambiadas"`
 }
