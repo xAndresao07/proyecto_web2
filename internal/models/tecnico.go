a691301eb97367be1e4f323c08650e448b09ea1c
Anthol34 <josue10anthony1@gmail.com>
2026-07-08 17:01:18 -0500

    Revert "demo: fallo de compilacion CP2"
    This reverts commit 0fe821ec565fd74516e23577b4eea28e2f9c26fc.

--- internal/models/tecnico.go
+++ internal/models/tecnico.go
@@ -2,7 +2,7 @@ package models
 
 type Tecnico struct {
 	ID         int                `json:"id" gorm:"primaryKey"`
-	Nombree    string             `json:"nombre" gorm:"not null"`
+	Nombre     string             `json:"nombre" gorm:"not null"`
 	Reputacion float64            `json:"reputacion"`
 	Servicios  []ServicioOfrecido `json:"servicios" gorm:"foreignKey:TecnicoID"`
 	Horarios   []HorarioTecnico   `json:"horarios" gorm:"foreignKey:TecnicoID"`
